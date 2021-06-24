b33aef928e457f22911ebbf5fb841b10da5cdddd
Matus Jan Lavko <matusjanlavko@gmail.com>
2021-06-24 11:27:38 +0200

    added estimation methods

--- portfolio.py
+++ portfolio.py
@@ -1,20 +1,12 @@
 import numpy as np
 import yfinance as yf
 
-class portfolio:
-    def __init__(self, securities: list, weights = None):
+class Engine:
+    def __init__(self, securities: list):
         self.securities = securities
         self.size = int(len(self.securities))
-        self.weights = None
         self.prices = None
         self.period = None
-
-        if self.weights is not None:
-            self.weights = weights
-        else:
-            #initialize to equal weights
-            self.weights = np.empty(self.size, dtype=float)
-            self.weights.fill(1/self.size)
 
     def __add__(self, other):
         raise NotImplementedError
@@ -33,5 +25,33 @@ class portfolio:
                                       start = self.period[0],
                                       end = self.period[1])
             self.prices = self.prices.loc[:, ('Adj Close', slice(None))]
+            self.returns = self.prices.pct_change()
         except AssertionError:
             print('You need to provide start and end dates!')
+
+    def _get_state(self, t_0, t_1):
+        #slicing the engine data structure
+        assert t_0 <= t_1
+        return self.returns.iloc[t_0 : t_1]
+
+class Portfolio(Engine):
+    def __init__(self, securities : list, start_weights = None):
+        super().__init__(securities)
+
+        self.start_weights = start_weights
+
+        if self.start_weights is not None:
+            self.start_weights = start_weights
+        else:
+            #initialize to equal weights
+            self.weights = np.empty(self.size, dtype=float)
+            self.weights.fill(1/self.size)
+
+    def set_transaction_cost(self, transaction_cost = '0.005'):
+        self.transaction_cost = transaction_cost
+
+
+
+
+
+
--- portfolio_test.py
+++ portfolio_test.py
@@ -1,15 +1,15 @@
-from portfolio import portfolio
+from portfolio import Portfolio
 import pandas as pd
 import matplotlib.pyplot as plt
 
 def main():
     securities = ['SPY', 'BND', 'GLD', 'NDX']
-    test = portfolio(securities)
+    test = Portfolio(securities)
     test.set_period(('2015-01-01', '2021-05-28'))
     test.get_prices('daily')
-    test.prices.plot()
-    plt.show()
 
+    (1+test._get_state(0,50)).cumprod().plot()
+    plt.show()
 if __name__ == '__main__':
     main()
 
