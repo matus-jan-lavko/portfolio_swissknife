b3bfdd8c1bea279285860ce29dfdcd6130007037
Matus Jan Lavko <matusjanlavko@gmail.com>
2021-07-11 13:08:08 +0200

    examples pkg rewrite

--- .virtual_documents/examples/02_portfolio_construction.ipynb.py
+++ .virtual_documents/examples/02_portfolio_construction.ipynb.py
@@ -7,34 +7,24 @@ get_ipython().run_line_magic("autoreload", " 2")
 plt.style.use('bmh')
 
 
-os.chdir('C:\\Users\matus\pysource\portfolio-swissknife')
-
-
-from portfolio import Portfolio
-from risk_model import RiskModel
-import estimation as est
-import optimization as opt
-import plotting
-import metrics
+from portfolio_swissknife import portfolio as ps
+from portfolio_swissknife import risk_model as rm
 
 
 #loading ext. data -- close prices of SPX
-universe = pd.read_csv('ext_data/00_db_SPX__PX_LAST.csv', index_col = 0, parse_dates = True)
+universe = pd.read_csv('../ext_data/00_db_SPX__PX_LAST.csv', index_col = 0, parse_dates = True)
 universe = universe[::-1].loc[:,universe.notna().all(axis=0)]
 securities = [universe.columns[i].split(' ')[0] for i, _ in enumerate(universe.columns)]
 universe = universe.loc['2014':]
 
 
-port_universe = Portfolio(securities)
+port_universe = ps.Portfolio(securities)
 port_universe.set_custom_prices(universe, 'daily')
 
 
 factors = ['SPY', 'VLUE', 'SIZE', 'QUAL', 'MTUM', 'USMV']
-rm_universe = RiskModel(port_universe, factors)
+rm_universe = rm.RiskModel(port_universe, factors)
 rm_universe.get_prices('daily')
-
-
-rm_universe.returns.shape
 
 
 a, b, c = rm_universe._estimate_panel(method='linear')
